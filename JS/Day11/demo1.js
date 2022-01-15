aa798961d10484d123ed36497f4f48d99c216b35
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-01-15 19:55:25 +0530

    Day11

---
+++ JS/Day11/demo1.js
@@ -0,0 +1,83 @@
+// console.log("Welcome to JS");
+
+// document.write('Welcome to JS');
+
+var a;
+a=2.5;
+// console.log('Value of a is '+a);
+a="Sumit";
+// console.log('Value of a is '+a);
+a=true
+// console.log('Value of a is '+a)
+
+let a1;
+//var has global 
+// let has scope withinin the nearest block ({})
+//operators
+//Airithematic +,-,/,*,%
+// assignment =,+=,-=
+//logical &&,||,!
+//bitwise & ,|,^,~,!,<<,>>
+// relational => !=,<=,>=,==, ===(Strongly equality it checks data type as well a its value )
+// a===b
+//unary pre inc,post ,++/--
+//ternary => condition?expression1:expression2
+
+//control statement
+// if,if-else,if ladder,switch,break,continue,nested if-else
+// if(21<4){
+//   console.log("Condition is true..");
+// }else{
+//   console.log("Condition is false");
+// }
+
+var choice=2;
+
+// switch(choice){
+//   case 1:console.log("U r in case 1");
+//            break;
+//   case 2: console.log("U r in Case 2");
+//           break;
+//   default:console.log('U r in default case...');
+// }
+
+// loop
+// for loop,while,do-while
+
+var count=5;
+// while(count!=0){
+//     console.log("Count is "+count);
+//     count--;
+// }
+
+// do{
+//   console.log("Count is "+count);
+//     count--;
+// }while(count!=0)
+
+//for loop 
+// 
+// for(int i=0;i<5;i++){
+    // logic
+// }
+
+// for(var i=0;i<5;i++){
+//     console.log("Value of i is "+i)
+// }
+
+for (let i = 0; i < 5; i++) {
+      console.log("Value of i is "+i);
+}
+// console.log("After loop i is "+i)
+
+
+
+
+
+
+
+
+
+
+
+

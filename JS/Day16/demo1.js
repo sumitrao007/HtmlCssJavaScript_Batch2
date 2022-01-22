6b45e61a3cc1b55a8b0c3d9a89e9d7795a5f7218
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2022-01-22 19:59:46 +0530

    Day16

---
+++ JS/Day16/demo1.js
@@ -0,0 +1,51 @@
+
+function validation(){
+  var gender=document.getElementsByName("gender");
+  
+  var Checkbox=document.getElementsByName("Checkbox");
+
+  var technoSpan=document.getElementById("technoSpan");
+  var cnt=0;
+
+  var genderSpan=document.getElementById("genderid");
+
+
+
+  var city=document.getElementById("city").value;
+
+
+  if(gender[0].checked==false && gender[1].checked==false){
+    // alert("Please Select Gender");
+    genderSpan.innerHTML="** Please Select the Gender!!"
+ }else{
+   // alert("Thank You For Gender Selection!!!!");
+ }
+
+ for(let i=0;i<Checkbox.length;i++){
+  if(Checkbox[i].checked==true){
+      cnt++;
+  }
+}
+
+if(cnt==0){
+   // alert("Please Select at least one CheckBox!!!");
+   technoSpan.innerHTML="**Please Select at least one Technology...."
+}else{
+   // alert("Thank for Technology Selection");
+}
+
+
+if(city==="selectcity"){
+  alert("Please Select one option from drop down box!!!");
+}else{
+  alert("Thank you form city Selection "+city);
+}
+
+}
+
+
+function onClick(){
+
+  document.getElementById("genderid").innerHTML='';
+
+}
